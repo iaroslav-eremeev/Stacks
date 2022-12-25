6612a1658e1ce8aa757fbbc17c381744e8cf5e8f
Iaroslav <eremeev.pt@gmail.com>
2022-12-25 12:34:35 +0200

    Separate Stacks and Queue projects

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -1,6 +1,5 @@
 package program;
 
-import model.Queue;
 import model.Stack;
 import util.Util;
 
@@ -10,7 +9,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Stack<String> stack = new Stack<>();
-        Queue<String> queue = new Queue<>();
         System.out.println("Enter 3 strings for a new stack");
         stack.put(scanner.next());
         stack.put(scanner.next());
@@ -19,14 +17,7 @@ public class Program {
         System.out.println("Now we use get on stack. See what changed");
         stack.get();
         System.out.println(stack);
-        System.out.println("Enter 3 strings for a new queue");
-        queue.put(scanner.next());
-        queue.put(scanner.next());
-        queue.put(scanner.next());
-        System.out.println(queue);
-        System.out.println("Now we use get on queue. See what changed");
-        queue.get();
-        System.out.println(queue);
+
         System.out.println("Now we calculate factorial of a big number. Enter such a big number");
         System.out.println("Its factorial is: " + Util.bigFactorial(scanner.nextInt()));
     }
